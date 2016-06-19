3de274ef74cd2607195c8740587181a159f4c9f2
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2016-06-19 19:54:19 -0400

    Read the default numeric system for locales

--- core/src/main/scala/scala/scalajs/locale/ldml/ldml.scala
+++ core/src/main/scala/scala/scalajs/locale/ldml/ldml.scala
@@ -5,19 +5,22 @@ import java.util.Locale
 /**
   * Interfaces describing the digit symbols
   */
-case class LDMLDigitSymbols()
+case class LDMLNumberingSystem(id: String)
+
+case class LDMLDigitSymbols(system: LDMLDigitSymbols)
 
 /**
   * Interfaces describing an LDML Locale
   */
 case class LDMLLocale(language: String, territory: Option[String],
-                      variant: Option[String], script: Option[String])
+    variant: Option[String], script: Option[String])
 
 /**
   * Wrapper to LDML
   */
-case class LDML(parent: Option[LDML], locale: LDMLLocale) {
-  def languageTag: String = toLocale.toLanguageTag()
+case class LDML(parent: Option[LDML], locale: LDMLLocale, defaultNS: Option[LDMLNumberingSystem],
+    digitSymbols: Map[LDMLNumberingSystem, LDMLDigitSymbols] = Map.empty) {
+  def languageTag: String = toLocale.toLanguageTag
 
   def toLocale: Locale = {
     new Locale.Builder().setLanguage(locale.language)
