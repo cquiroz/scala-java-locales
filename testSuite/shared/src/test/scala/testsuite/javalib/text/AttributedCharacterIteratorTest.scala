2bc10e651e7294205f24bf69964e6c4aea534793
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2016-07-11 16:48:17 -0400

    Add java.text.AttributedCharacterIterator interface

---
+++ testSuite/shared/src/test/scala/testsuite/javalib/text/AttributedCharacterIteratorTest.scala
@@ -0,0 +1,19 @@
+package testsuite.javalib.text
+
+import java.text.AttributedCharacterIterator.Attribute
+
+import org.junit.Assert._
+import org.junit.Test
+
+class AttributedCharacterIteratorTest {
+  @Test def test_static_value_to_string(): Unit = {
+    assertEquals("java.text.AttributedCharacterIterator$Attribute(language)", Attribute.LANGUAGE.toString)
+    assertEquals("java.text.AttributedCharacterIterator$Attribute(reading)", Attribute.READING.toString)
+    assertEquals("java.text.AttributedCharacterIterator$Attribute(input_method_segment)", Attribute.INPUT_METHOD_SEGMENT.toString)
+  }
+
+  @Test def test_equals(): Unit = {
+    assertEquals(Attribute.LANGUAGE, Attribute.LANGUAGE)
+    assertFalse(Attribute.READING == Attribute.LANGUAGE)
+  }
+}
