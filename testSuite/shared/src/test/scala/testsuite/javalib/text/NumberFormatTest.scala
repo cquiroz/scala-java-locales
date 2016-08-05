ab2253ea9b8bfbd56832c565b155745c7f89d42a
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2016-08-04 20:05:40 -0400

    Tests for NumberFormat on extra locales

--- testSuite/shared/src/test/scala/testsuite/javalib/text/NumberFormatTest.scala
+++ testSuite/shared/src/test/scala/testsuite/javalib/text/NumberFormatTest.scala
@@ -3,34 +3,64 @@ package testsuite.javalib.text
 import java.text.{DecimalFormat, DecimalFormatSymbols, NumberFormat}
 import java.util.Locale
 
+import locales.LocaleRegistry
+import locales.cldr.LDML
+import locales.cldr.data._
 import org.junit.Test
 import org.junit.Assert._
 import testsuite.utils.{LocaleTestSetup, Platform}
 
 class NumberFormatTest extends LocaleTestSetup {
-  case class TestCase(tag: String, l: Locale, cldr21: Boolean, nf: String, inf: String, pf: String)
+  case class TestCase(tag: String, ldml: LDML, l: Locale, cldr21: Boolean, nf: String, inf: String, pf: String)
 
   val stdLocales = List(
-    TestCase("und", Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("en", Locale.ENGLISH, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("fr", Locale.FRENCH, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
-    TestCase("de", Locale.GERMAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
-    TestCase("it", Locale.ITALIAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("ko", Locale.KOREAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("zh", Locale.CHINESE, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("zh", Locale.SIMPLIFIED_CHINESE, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("zh-TW", Locale.TRADITIONAL_CHINESE, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("fr-FR", Locale.FRANCE, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
-    TestCase("de-DE", Locale.GERMANY, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
-    TestCase("it-IT", Locale.ITALY, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("ko-KO", Locale.KOREA, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("zh-CH", Locale.CHINA, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("zh-CH", Locale.PRC, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("zh-TW", Locale.TAIWAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("en-GB", Locale.UK, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("en-US", Locale.US, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("en-CA", Locale.CANADA, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
-    TestCase("fr-CA", Locale.CANADA_FRENCH, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%")
+    TestCase("und", root, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("en", root, Locale.ENGLISH, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("fr", root, Locale.FRENCH, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
+    TestCase("de", root, Locale.GERMAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
+    TestCase("it", root, Locale.ITALIAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("ko", root, Locale.KOREAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh", root, Locale.CHINESE, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh", root, Locale.SIMPLIFIED_CHINESE, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh-TW", root, Locale.TRADITIONAL_CHINESE, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("fr-FR", root, Locale.FRANCE, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
+    TestCase("de-DE", root, Locale.GERMANY, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
+    TestCase("it-IT", root, Locale.ITALY, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("ko-KO", root, Locale.KOREA, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh-CH", root, Locale.CHINA, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh-CH", root, Locale.PRC, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh-TW", root, Locale.TAIWAN, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("en-GB", root, Locale.UK, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("en-US", root, Locale.US, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("en-CA", root, Locale.CANADA, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("fr-CA", root, Locale.CANADA_FRENCH, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%")
+  )
+
+  val extraLocales = List(
+    TestCase("af", af, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("az", az, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("az-Cyrl", az_Cyrl, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("it-CH", it_CH, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh", zh, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("zh-Hant", zh_Hant, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("fa", fa, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("fi-FI", fi_FI, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
+    TestCase("ja", ja, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("ka", ka, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%"),
+    TestCase("lv", lv, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("my", my, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("smn", smn, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("smn-FI", smn_FI, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("ru-RU", ru_RU, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0\u00A0%")
+  )
+
+  val extraLocalesDiff = List(
+    TestCase("ar", ar, Locale.ROOT, cldr21 = true, "#,##0.###;#,##0.###-", "#,##0;#,##0-", "#,##0%"),
+    TestCase("ar", ar, Locale.ROOT, cldr21 = false, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("bn", bn, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("bn", bn, Locale.ROOT, cldr21 = false, "#,##,##0.###", "#,##,##0", "#,##,##0%"),
+    TestCase("es-CL", es_CL, Locale.ROOT, cldr21 = true, "#,##0.###", "#,##0", "#,##0%"),
+    TestCase("es-CL", es_CL, Locale.ROOT, cldr21 = false, "#,##0.###", "#,##0", "#,##0 %")
   )
 
   @Test def test_constants(): Unit = {
@@ -42,6 +72,10 @@ class NumberFormatTest extends LocaleTestSetup {
     val nf = NumberFormat.getNumberInstance.asInstanceOf[DecimalFormat]
     assertEquals("#,##0.###", nf.toPattern)
     assertEquals(DecimalFormatSymbols.getInstance(), nf.getDecimalFormatSymbols)
+
+    val inf = NumberFormat.getIntegerInstance.asInstanceOf[DecimalFormat]
+    assertEquals("#,##0", inf.toPattern)
+    assertEquals(DecimalFormatSymbols.getInstance(), inf.getDecimalFormatSymbols)
 
     val pf = NumberFormat.getPercentInstance.asInstanceOf[DecimalFormat]
     assertEquals("#,##0%", pf.toPattern)
@@ -65,4 +99,49 @@ class NumberFormatTest extends LocaleTestSetup {
       assertEquals(DecimalFormatSymbols.getInstance(t.l), pf.getDecimalFormatSymbols)
     }
   }
+
+  @Test def test_extra_locales(): Unit = {
+    extraLocales.foreach { t =>
+      if (!Platform.executingInJVM) {
+        LocaleRegistry.installLocale(t.ldml)
+      }
+      val l = Locale.forLanguageTag(t.tag)
+      val nf = NumberFormat.getNumberInstance(l).asInstanceOf[DecimalFormat]
+      assertEquals(t.nf, nf.toPattern)
+      assertFalse(nf.isParseIntegerOnly)
+      assertEquals(DecimalFormatSymbols.getInstance(l), nf.getDecimalFormatSymbols)
+
+      val inf = NumberFormat.getIntegerInstance(l).asInstanceOf[DecimalFormat]
+      assertEquals(t.inf, inf.toPattern)
+      assertTrue(inf.isParseIntegerOnly)
+      assertEquals(DecimalFormatSymbols.getInstance(l), inf.getDecimalFormatSymbols)
+
+      val pf = NumberFormat.getPercentInstance(l).asInstanceOf[DecimalFormat]
+      assertEquals(t.pf, pf.toPattern)
+      assertEquals(DecimalFormatSymbols.getInstance(l), pf.getDecimalFormatSymbols)
+    }
+  }
+
+  @Test def test_extra_locales_diff(): Unit = {
+    extraLocalesDiff.filter(t => (Platform.executingInJVM && t.cldr21) || (!Platform.executingInJVM && !t.cldr21)).foreach { t =>
+      if (!Platform.executingInJVM) {
+        LocaleRegistry.installLocale(t.ldml)
+      }
+      val l = Locale.forLanguageTag(t.tag)
+      val nf = NumberFormat.getNumberInstance(l).asInstanceOf[DecimalFormat]
+      assertEquals(t.nf, nf.toPattern)
+      assertFalse(nf.isParseIntegerOnly)
+      assertEquals(DecimalFormatSymbols.getInstance(l), nf.getDecimalFormatSymbols)
+
+      val inf = NumberFormat.getIntegerInstance(l).asInstanceOf[DecimalFormat]
+      assertEquals(t.inf, inf.toPattern)
+      assertTrue(inf.isParseIntegerOnly)
+      assertEquals(DecimalFormatSymbols.getInstance(l), inf.getDecimalFormatSymbols)
+
+      val pf = NumberFormat.getPercentInstance(l).asInstanceOf[DecimalFormat]
+      assertEquals(t.pf, pf.toPattern)
+      assertEquals(DecimalFormatSymbols.getInstance(l), pf.getDecimalFormatSymbols)
+    }
+  }
+
 }
