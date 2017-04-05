0fe03529eaf5c791bc92b484a2b87ab21ca969e6
andrea <andrea.peruffo1982@gmail.com>
2017-04-05 19:19:36 +0100

    Platform dependent testing of dates

---
+++ macroUtils/src/main/scala/JVMDate.scala
@@ -0,0 +1,37 @@
+package io.github.cquiroz.utils
+
+import java.util.Date
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
+
+object JVMDate {
+  type JVMDateT = {
+    val year: Int
+    val month: Int
+    val date: Int
+    val hours: Int
+    val minutes: Int
+    val seconds: Int
+  }
+
+  def getImpl(c: Context)(time: c.Expr[Long]) = {
+    import c.universe._
+
+    val timeValue = TreeHelper.resetLong(c)(time)
+
+    val date = new Date(c.eval[Long](timeValue))
+
+    c.Expr[JVMDateT](q"""
+      new {
+        val year: Int = ${date.getYear}
+        val month: Int = ${date.getMonth}
+        val date: Int = ${date.getDate}
+        val hours: Int = ${date.getHours}
+        val minutes: Int = ${date.getMinutes}
+        val seconds: Int = ${date.getSeconds}
+      }
+    """)
+  }
+
+  def get(time: Long): JVMDateT = macro getImpl
+}
--- testSuite/shared/src/test/scala/testsuite/javalib/text/SimpleDateFormatTest.scala
+++ testSuite/shared/src/test/scala/testsuite/javalib/text/SimpleDateFormatTest.scala
@@ -6,60 +6,72 @@ import java.text.SimpleDateFormat
 import org.junit.Assert._
 import org.junit.Test
 
+import io.github.cquiroz.utils.JVMDate
+import scala.language.reflectiveCalls
+
 class SimpleDateFormatTest {
+  final val time = 1491381282242L
+  val date = new Date(time)
+  val jvmDate = JVMDate.get(time)
+  val year = totalSize((1900 + jvmDate.year), 4)
+  val month = totalSize(1 + jvmDate.month, 2)
+  val day = totalSize(jvmDate.date, 2)
+  val hours = totalSize(jvmDate.hours, 2)
+  val minutes = totalSize(jvmDate.minutes, 2)
+  val seconds = totalSize(jvmDate.seconds, 2)
+  val millis = totalSize(date.getTime - Date.UTC(date.getYear,
+     date.getMonth,
+     date.getDay,
+     date.getHours,
+     date.getMinutes,
+     date.getSeconds), 3)
+
+  def totalSize(num: Long, size: Int) = {
+    val base = num.toString
+    val trail = (for (_ <- 0 until (size - base.size)) yield '0').toArray
+
+    if (base.size > size) base.substring(base.size - 3, base.size)
+    else new String(trail) + base
+  }
+
   @Test def test_year_format(): Unit = {
     val f = new SimpleDateFormat("yyyy")
-
-    val d = new Date(1491381282242L)
-    assertEquals("2017", f.format(d))
+    assertEquals(year, f.format(date))
   }
 
   @Test def test_month_format(): Unit = {
     val f = new SimpleDateFormat("MM")
-
-    val d = new Date(1491381282242L)
-    assertEquals("04", f.format(d))
+    assertEquals(month, f.format(date))
   }
 
   @Test def test_day_format(): Unit = {
     val f = new SimpleDateFormat("dd")
-
-    val d = new Date(1491381282242L)
-    assertEquals("05", f.format(d))
+    assertEquals(day, f.format(date))
   }
 
   @Test def test_hour_format(): Unit = {
     val f = new SimpleDateFormat("HH")
-
-    val d = new Date(1491381282242L)
-    assertEquals("08", f.format(d))
+    assertEquals(hours, f.format(date))
   }
 
   @Test def test_minutes_format(): Unit = {
     val f = new SimpleDateFormat("mm")
-
-    val d = new Date(1491381282242L)
-    assertEquals("34", f.format(d))
+    assertEquals(minutes, f.format(date))
   }
 
   @Test def test_seconds_format(): Unit = {
     val f = new SimpleDateFormat("ss")
-
-    val d = new Date(1491381282242L)
-    assertEquals("42", f.format(d))
+    assertEquals(seconds, f.format(date))
   }
 
   @Test def test_millis_format(): Unit = {
     val f = new SimpleDateFormat("SSS")
-
-    val d = new Date(1491381282242L)
-    assertEquals("242", f.format(d))
+    assertEquals(millis, f.format(date))
   }
 
   @Test def test_full_date_format(): Unit = {
     val f = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:SSS")
-
-    val d = new Date(1491381282242L)
-    assertEquals("05/04/2017 08:34:42:242", f.format(d))
+    assertEquals(s"$day/$month/$year $hours:$minutes:$seconds:$millis",
+      f.format(date))
   }
 }
