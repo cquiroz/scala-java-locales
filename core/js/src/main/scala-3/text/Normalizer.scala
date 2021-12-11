079486d7f463d38c02745a132d3972300dd19e77
Arman Bilge <armanbilge@gmail.com>
2021-12-11 02:58:31 +0000

    Fix Scala 3 compile, again

--- core/js/src/main/scala-3/text/Normalizer.scala
+++ core/js/src/main/scala-3/text/Normalizer.scala
@@ -4,13 +4,13 @@ import scala.scalajs.js
 
 object Normalizer {
 
-  inline def normalize(src: CharSequence, form: Form): String =
+  @inline def normalize(src: CharSequence, form: Form): String =
     if (src == null || form == null)
       throw new NullPointerException
     else
       src.toString().asInstanceOf[js.Dynamic].normalize(form.name()).asInstanceOf[String]
 
-  inline def isNormalized(src: CharSequence, form: Form): Boolean =
+  @inline def isNormalized(src: CharSequence, form: Form): Boolean =
     normalize(src, form).contentEquals(src)
 
   enum Form extends java.lang.Enum[Form]:
